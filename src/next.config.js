5261dcb447f66a5e7d68c58b49c4241d5b20f700
Purvik Anghan <purvikanghan1525@gmail.com>
2025-07-29 13:20:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.js
@@ -0,0 +1,25 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
